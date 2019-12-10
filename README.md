0cd3fafe99a04b9425cd50b5a51d709465bd7129
Maxi <maximiliano.cespedes@coolblue.nl>
2019-12-10 16:12:49 +0100

    buy me a coffee

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# Quotify is the best
+# Quotify is the best API ever
 
-## Buy my subscription
+> Buy my subscription
+
+## Install
+
+```yarn install```
+
+## Run
+
+```bash
+yarn serve
+
+// get your random quote by going to 'http://localhost:3000/
+```
--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.get("/", function(req, res) {
   res.json(quotes.list[Math.round(currentQuote)].content);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 
