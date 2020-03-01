5e0c27f9c460e5d04a1dfe0e6c9f29d6e83497e5
Max <maxicespedes7@hotmail.com>
2020-03-01 20:44:26 +0100

    test

--- __test__/index.spec.js
+++ __test__/index.spec.js
@@ -1,26 +1,30 @@
 describe('test', () => {
-  test('should fail the action', () => {
-    expect(true).toBe(false);
-  });
-  test('should fail the action 2', () => {
-    expect(true).toBe(false);
-  });
-  test('should fail the action 3', () => {
-    expect(true).toBe(false);
-  });
-  test('should fail the action 4', () => {
-    expect(true).toBe(false);
-  });
-  test('should fail the action 5', () => {
-    expect(true).toBe(false);
-  });
-  test('should fail the action 6', () => {
-    expect(true).toBe(false);
-  });
+  // test('should fail the action', () => {
+  //   expect(true).toBe(false);
+  // });
+  // test('should fail the action 2', () => {
+  //   expect(true).toBe(false);
+  // });
+  // test('should fail the action 3', () => {
+  //   expect(true).toBe(false);
+  // });
+  // test('should fail the action 4', () => {
+  //   expect(true).toBe(false);
+  // });
+  // test('should fail the action 5', () => {
+  //   expect(true).toBe(false);
+  // });
+  // test('should fail the action 6', () => {
+  //   expect(true).toBe(false);
+  // });
+  // test('should fail the action 7', () => {
+  //   expect(true).toBe(false);
+  // });
+  // test('should fail the action 7', () => {
+  //   expect(true).toBe(false);
+  // });
+
   test('should fail the action 7', () => {
-    expect(true).toBe(false);
-  });
-  test('should fail the action 7', () => {
-    expect(true).toBe(false);
+    expect(true).toBe(true);
   });
 });
